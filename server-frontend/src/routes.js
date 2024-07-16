95c27b2a6d3003aad941e95157f9190a99c457c4
Brent <brenthosie@gmail.com>
2024-07-16 11:31:32 -0600

    change to docker

--- server-frontend/src/routes.js
+++ server-frontend/src/routes.js
@@ -1,12 +1,10 @@
-const API_URL = import.meta.env.VITE_API_URL;
-
 export async function getRecommendation(movieTitle) {
     if (!movieTitle) {
         return Promise.reject('no movie title supplied')
     }
 
     try {
-        const response = await fetch(`${API_URL}/recommendation/${movieTitle}`); // Fetch data from the same server
+        const response = await fetch(`/recommendation/${movieTitle}`); // Fetch data from the same server
         return await response.json();
     } catch (error) {
         return Promise.reject(error)
@@ -16,7 +14,7 @@ export async function getRecommendation(movieTitle) {
 export async function getMovieTitles() {
     console.log('APIURL', API_URL)
     try {
-        const response = await fetch(`${API_URL}/get-movie-titles`); // Fetch data from the same server
+        const response = await fetch('/get-movie-titles'); // Fetch data from the same server
         const { movie_titles } = await response.json();
         return movie_titles;
     } catch (error) {
