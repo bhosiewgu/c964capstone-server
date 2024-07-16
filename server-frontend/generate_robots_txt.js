96415e2ccb490acd8f9e9f614aed660c5fa5a552
Brent <brenthosie@gmail.com>
2024-07-16 00:45:57 -0600

    attempt to generate robots

---
+++ server-frontend/generate_robots_txt.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+const path = require('path')
+
+const robotsTxtContent = `User-agent: *
+Disallow: /`
+
+const pathToWWW = path.resolve(__dirname, '..', 'www')
+
+fs.writeFleSync(`${pathToWWW}/robots.txt`, robotsTxtContent)
